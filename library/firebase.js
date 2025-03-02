d4b39ee9a11efaff1216fc7ad35d391428423fc2
Christopher Cole <cmc8066@psu.edu>
2025-03-02 18:27:19 -0500

    refactored code for privacy

--- library/firebase.js
+++ library/firebase.js
@@ -3,19 +3,19 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyChbw1dM8PPD3QA9jmXA5siOn8OUz-qOqk",
-    authDomain: "snappersstudyspace-a5cff.firebaseapp.com",
-    projectId: "snappersstudyspace-a5cff",
-    storageBucket: "snappersstudyspace-a5cff.firebasestorage.app",
-    messagingSenderId: "372810280107",
-    appId: "1:372810280107:web:2f44197aaa19966a095bee",
-    measurementId: "G-QPVLEWHJER"
-  };
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const database = getFirestore(app);
-export const googleProvider = new GoogleAuthProvider(app).addScope('email');
+export const googleProvider = new GoogleAuthProvider().addScope('email');
 
-export default app
+export default app;
